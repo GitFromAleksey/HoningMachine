60f09286aa03c1defcbc9362655e1c233832c1b2
Aleksei <borzenkov@eag.su>
2020-03-19 14:37:39 +0700

    комит из Eclipce

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 19 мар. 2020 г.
+ *      Author: borzenkov
+ */
+
+#include <iostream>
+//#include <std>
+
+int main()
+{
+	std::cout << "Hi!!!" << std::endl;
+
+}
+
+
