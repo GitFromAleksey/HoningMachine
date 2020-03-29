b0a8bc0ce021f5637c64c6e65d9965f21148871d
John Doe <johndoe@example.com>
2020-03-29 23:58:31 +0700

    Удалось настроить асинхронную передачу и приём UART

--- keil/Src/main.cpp
+++ keil/Src/main.cpp
@@ -50,6 +50,8 @@ volatile uint16_t adcDmaData[2];
 ADC_HandleTypeDef hadc1;
 DMA_HandleTypeDef hdma_adc1;
 
+UART_HandleTypeDef huart1;
+
 /* USER CODE BEGIN PV */
 #define I_PROCESS_ARRAY_SIZE	12
 uint8_t iProcessArrCnt = 0;
@@ -80,6 +82,7 @@ static void MX_GPIO_Init(void);
 static void MX_ADC1_Init(void);
 static void MX_DMA_Init(void);
 static void MX_NVIC_Init(void);
+static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 void DO_SwitchCallback(void *port, uint16_t pinNumber, bool hi_lo);
 bool DO_CheckStateCallback(void *port, uint16_t pinNumber);
@@ -201,7 +204,7 @@ int main(void)
   MX_GPIO_Init();
 	MX_DMA_Init();
 	MX_ADC1_Init();
-
+  MX_USART1_UART_Init();
   /* Initialize interrupts */
 //  MX_NVIC_Init();
 	
@@ -220,9 +223,17 @@ int main(void)
 	
 	//HAL_ADC_Start_IT(&hadc1);
 	HAL_ADC_Start_DMA(&hadc1, (uint32_t*)&adcDmaData, 2);
-	
+	HAL_StatusTypeDef st;
+	uint8_t uartData[10];
+
   while (1)
   {
+		
+		st = HAL_UART_Receive(&huart1, uartData, sizeof(uartData), 0x100);
+		//if(st != HAL_TIMEOUT)
+			HAL_UART_Transmit(&huart1, uartData, sizeof(uartData), 100);
+// (UART_HandleTypeDef *huart, uint8_t *pData, uint16_t Size, uint32_t Timeout)
+		
 //		if(DI.IsOn())	
 //			DO.SetOff();
 //		else					
@@ -346,6 +357,39 @@ static void MX_ADC1_Init(void)
   /* USER CODE END ADC1_Init 2 */
 
 }
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
 /** 
   * Enable DMA controller clock
   */
