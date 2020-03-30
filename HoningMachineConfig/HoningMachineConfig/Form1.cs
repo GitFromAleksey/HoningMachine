ec68f34d6d2701cd48c9fbf142d58eaa28beea91
John Doe <johndoe@example.com>
2020-03-30 20:32:44 +0700

    Сделан мерж с веткой keil Добавлен проект HoningMachineConfig для PC

---
+++ HoningMachineConfig/HoningMachineConfig/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace HoningMachineConfig
+{
+    public partial class MainForm1 : Form
+    {
+        public MainForm1()
+        {
+            InitializeComponent();
+            // разворачиваем форму на весь экран
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        // вывод списка com портов
+        private void portToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStripComboBox1.Items.Clear();
+            toolStripComboBox1.Items.AddRange(SerialPort.GetPortNames());
+        }
+    }
+}
